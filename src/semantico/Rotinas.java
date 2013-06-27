df88a6fca149814789a2042b92864cc48fa8f6c3
gustavoluvizotto@gmail.com <gustavoluvizotto@gmail.com@b810e687-23ee-fa9a-7a26-d0a6704c44bb>
2013-06-27 23:53:28 +0000

    rotinas 0, 2, 3, 4 e 9

--- src/semantico/Rotinas.java
+++ src/semantico/Rotinas.java
@@ -14,19 +14,33 @@ public class Rotinas {
     }
 
     /**
-     * Rotina 0, 2, 3. Verifica se ident ja esta declarado na TS
-     *
-     * @param id
-     * @param nivelcorr
+     * Rotina 0, 2, 3 e 4. Se declarado(TS,s,nivelcorr) então 
+     * erro(‘id já declarado’); senão insere(TS,s,ref);
+     * Atualiza os campos nível, categoria, tipo_v das variáveis na TS através 
+     * da rotina seta_atributos com: nivelcorr, variável, o endereço fornecido 
+     * pela rs(9);
+     * @param id identificador a ser verificado (s)
+     * @param nivelcorr nível a ser buscado
      */
     public void verificaEInsereID(String id, int nivelcorr) {
+        Descritor desc = new Descritor(id, nivelcorr);
         if (TS.declarado(id, nivelcorr)) {
             System.err.println("id já declarado");
         } else {
-            TS.insere(id);
+            desc.setCategoria("var");
+            desc.setEnder(this.rotinaNove(id, nivelcorr, "int"));
+            TS.insere(desc, nivelcorr);
         }
     }
 
+    /**
+     * Rotina 9. busca(TS,s,ref,achou); Se não achou ou (achou e categoria <> tipo) 
+     * então erro(‘tipo não definido’) End_tipo:= ref
+     * @param id identificador 
+     * @param nivelcorr nivel atual
+     * @param tipo 
+     * @return 
+     */
     public String rotinaNove(String id, int nivelcorr, String tipo) {
         Descritor achou = TS.busca(id); // essa busca tem que ser feita no nivelcorr
         if (achou == null || !achou.getCategoria().equals(tipo)) {
@@ -34,8 +48,7 @@ public class Rotinas {
         }
         return achou.getEnder();
     }
-    public void preencheVarNaTS(Descritor des) {
-        
-    }
+    
+    // ATRIBUTOS
     private TabelaSimbolos TS;
 }
